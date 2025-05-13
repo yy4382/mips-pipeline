223ec4e8903ee5faf9f345695e97925c9dd1659c
Chris Yang <yy4382@outlook.com>
2025-05-14 01:13:24 +0800

    chore: remove `console.log`

--- src/components/tomasulo/tomasulo-core.tsx
+++ src/components/tomasulo/tomasulo-core.tsx
@@ -34,7 +34,6 @@ export function TomasuloCoreDisplay({
     );
   }, [coreStatus, insts]);
   useEffect(() => {
-    console.log("coreStatus effect", coreStatus.length);
     setShowingClockCycle(coreStatus.length > 0 ? coreStatus.length - 1 : 0);
   }, [coreStatus]);
 
